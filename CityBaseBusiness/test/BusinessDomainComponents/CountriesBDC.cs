6731dd0c64856de0130695ee5dae2d4828cbe0ac
Ketan Arora <ketan.arora@paytmmall.com>
2019-11-16 23:17:02 +0530

    - Business and Data Layer implented - Mock countries and states provided

---
+++ CityBaseBusiness/test/BusinessDomainComponents/CountriesBDC.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using CityBaseShared.DataTransferObjects;
+namespace CityBaseBusiness.BusinessDomainComponents
+{
+    public class CountriesBDC
+    {
+        public CountriesBDC()
+        {
+        }
+
+        //Get list of all countries
+        public IEnumerable<CountryDTO> Get()
+        {
+            IList<CountryDTO> countries = new List<CountryDTO>();
+            for(var i = 0; i<=5; i++)
+            {
+                var country = new CountryDTO {
+                    CountryId = i,
+                    CountryName = "Country" + i
+                };
+                countries.Add(country);
+            }
+            return countries;
+
+        }
+
+        //Get list of all states using a countryId
+        public IEnumerable<StateDTO> Get(int countryId)
+        {
+            IList<StateDTO> states = new List<StateDTO>();
+            for (var i = 0; i <= 5; i++)
+            {
+                var state = new StateDTO {
+                    StateId = i,
+                    CountryId = countryId,
+                    StateName = "State" + i
+                };
+                states.Add(state);
+            }
+            return states;
+
+        }
+    }
+}
